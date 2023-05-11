2505b74ceee1a9f3f9f66fdea8470cad208e6ded
Micke Prado <m.apg@hotmail.es>
2023-05-10 18:54:04 -0600

    Add project files.

---
+++ MakeControllerApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MakeControllerApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MakeControllerApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MakeControllerApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
